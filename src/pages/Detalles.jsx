83f5d1db087e37254b470f3958a91c1013fd3f82
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-08-18 23:09:31 -0300

    Detalles y buscador completados

--- src/pages/Detalles.jsx
+++ src/pages/Detalles.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useParams } from "react-router";
+import { Personajes } from "../models/Personajes";
 
-const Detalles = (props) => {
+const Detalles = ({history}) => {
   const { id } = useParams();
-  console.log(id);
+  const { type, name, description } = Personajes.find((x) => x.id === id);
+  const path = `/assets/${type}-${id}.png`;
+
+  const volver = () => {
+    history.goBack();
+  }
   return (
-    <div>
-      <h1>{id}</h1>
-    </div>
+    <>
+      <div className="conatainer mx-5 mt-4">
+        <div className="">
+          <div className="card mb-3 text-white bg-dark"  style={{ maxWidth: 540 }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={path} className="img-fluid rounded-start" alt={path} />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{name}</h5>
+                  <p className="card-text">
+                    {description}
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      {type}
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type="button" className="btn btn-outline-info" onClick={volver}> volver</button>
+
+      </div>
+    </>
   );
 };
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import queryString from "query-string";
+import { Personajes } from "../models/Personajes";
+import Card from "../components/Card";
 
-const Search = (props) => {
+const Search = ({ history }) => {
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const [inputValue, setInputValue] = useState(q);
+  const [characters, setCharacters] = useState([]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`?q=${inputValue}`);
+  };
+
+  const getCharacters = () => {
+    if (inputValue.trim() !== "") {
+      const value = inputValue.toLocaleLowerCase();
+      const newValue = Personajes.filter((character) =>
+        character.name.toLocaleLowerCase().includes(value)
+      );
+
+      setCharacters(newValue);
+    } else {
+      setCharacters([]);
+    }
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, [q]);
   return (
-    <div>
-      <h1>Buscador</h1>
-    </div>
+    <>
+      <div className=" container mx-5 mt-4">
+        <div className="row text-center">
+          <div className=" col-6">
+            <div className="card text-white bg-primary mb-3">
+              <div className="card-header">Buscador:</div>
+              <div className="card-body">
+                <h5 className="card-title">Introdusca nombre del empleado </h5>
+                <form onSubmit={handleSubmit}>
+                  <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">
+                      Buscar
+                    </span>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Username"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      value={inputValue}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <button type="submit" className="btn btn-outline-info w-100">
+                    Buscar
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div
+              className="card border-info  mb-3"
+            >
+              <div className="card-header text-info">Resultado</div>
+              <div className="card-body text-info ">
+                <h5 className="card-title">Cantidad: {characters.length}</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            {characters.length === 0 && (
+              <div className="alert alert-warning text-center">
+                Introdusca un nombre correcto
+              </div>
+            )}
+          </div>
+          <div className="row">
+            {characters.map((character) => (
+              <Card key={character.id} {...character} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
